3cc4a312d6917c2d2f97b6dcafaca2e7aa62d6a9
michael-gokey-architect <michael.gokey.architect@gmail.com>
2024-04-30 20:32:35 -0400

    built out basic app with modules and some components

---
+++ ng-sample-auth-fb/src/app/guest/guest.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GuestHomeComponent } from './guest-home/guest-home.component';
+
+
+
+@NgModule({
+  declarations: [
+    GuestHomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GuestModule { }
